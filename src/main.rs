76cc89845eabf824a148d1fd890f6f99034c874f
Jason Holm <holmjason2@gmail.com>
2019-07-26 17:53:21 -0700

    fixed html renderer

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,68 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
+use std::fs::File;
 use std::io;
+use std::io::Write;
+use std::path::Path;
 
-use rocket::response::Redirect;
-use rocket::response::NamedFile;
-use rocket::request::{Form, FromFormValue};
-use rocket::http::RawStr;
 use comrak::{markdown_to_html, ComrakOptions};
-use horrorshow::Raw;
 use horrorshow::helper::doctype;
-use horrorshow::html;
-
-#[derive(Debug)]
-struct ValidInput<'a>(&'a str);
+use horrorshow::{html, Raw};
+use rocket::http::RawStr;
+use rocket::request::Form;
+use rocket::response::{NamedFile, Redirect};
 
 #[derive(FromForm)]
-struct UserInput<'a> {
-    input: Result<ValidInput<'a>, &'static str>,
+struct UserInput {
+    input: String,
 }
 
-impl<'v> FromFormValue<'v> for ValidInput<'v> {
-    type Error = &'static str;
-
-    fn from_form_value(v: &'v RawStr) -> Result<Self, Self::Error> {
-        if v.len() == 0 {
-            Err("too short!")
-        } else {
-            Ok(ValidInput(v.as_str()))
-        }
+impl UserInput {
+    fn new(text: &str) -> UserInput {
+        let input = text.to_string();
+        UserInput { input }
     }
-}
 
-#[post("/markdown", data = "<text>")]
-fn markdown(text: Form<UserInput>) -> Result<Redirect, String> {
-    Ok(UserInput::render(format!("{}", UserInput::input)))
-}
-
-impl<'a> UserInput<'a> {
-    fn render(text: &RawStr) -> String {
-        format!("{}", html!(
-            : doctype::HTML;
-            html {
-                body {
-                    : Raw(markdown_to_html("# Hello", &ComrakOptions::default()))
+    fn render(&self, text: &str) -> String {
+        format!(
+            "{}",
+            html!(
+                : doctype::HTML;
+                html {
+                    body {
+                        : Raw(markdown_to_html(text, &ComrakOptions::default()))
+                    }
                 }
-            }
-        )
+            )
         )
     }
+}
+
+#[post("/", data = "<text>")]
+fn markdown(text: Form<UserInput>) -> Result<Redirect, File> {
+    let user = UserInput::new(text.input.as_str());
+    let rendered = user.render(&user.input);
+
+    let mut file = File::create(Path::new("display")).expect("Error");
+    file.write_all(rendered.as_bytes()).expect("Error");
+    Ok(Redirect::to("display"))
+}
+
+#[get("/<filename>")]
+fn display(filename: &RawStr) -> Result<File, std::io::Error> {
+    File::open(filename.as_str())
 }
 
 #[get("/")]
-pub fn index() -> io::Result<NamedFile> {
+fn index() -> io::Result<NamedFile> {
     NamedFile::open("static/index.html")
 }
 
 fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/", routes![index, markdown])
+    rocket::ignite().mount("/", routes![index, markdown, display])
 }
 
 fn main() {
